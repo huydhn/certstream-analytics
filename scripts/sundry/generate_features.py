bb8d99fc944b27064122cdabd4c90f4c3932b9a8
Huy Do <huydhn@gmail.com>
2019-04-24 15:45:59 +0800

    Some minor tweaks in generate_features sundry script.

--- scripts/sundry/generate_features.py
+++ scripts/sundry/generate_features.py
@@ -5,21 +5,37 @@ Generate features for outlier detection.
 import json
 import sys
 
+from certstream_analytics.analysers import WordSegmentation
+from certstream_analytics.analysers import IDNADecoder
 from certstream_analytics.analysers import FeaturesGenerator
 
-def main():
+def main(max_count=None):
     '''
     The record is assumed to be stored in a JSON file passed in as the first
     parameter of the script.
     '''
+    segmenter = WordSegmentation()
+    decoder = IDNADecoder()
     generator = FeaturesGenerator()
 
     with open(sys.argv[1]) as fhandle:
+        count = 0
+
         for line in fhandle:
-            record = json.loads(line.strip())
+            try:
+                record = json.loads(line.strip())
+            except json.decoder.JSONDecodeError:
+                continue
+
+            record = decoder.run(record)
+            record = segmenter.run(record)
             record = generator.run(record)
 
             print(json.dumps(record))
+            count += 1
+
+            if max_count and count > max_count:
+                break
 
 
 if __name__ == '__main__':
